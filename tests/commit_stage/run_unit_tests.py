361916289000c4a1219b290174c54ae718b74d31
Jakub Kolodziejczyk <jakub.kolodziejczyk@mail.com>
2018-04-15 16:49:50 +0900

    Reworked unit tests script to python
    Switched from bash implementation to Python implementation

---
+++ tests/commit_stage/run_unit_tests.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+import pytest
+
+
+def main():
+    return pytest.main()
+
+
+if __name__ == "__main__":
+    main()
--- tests/commit_stage/run_unit_tests.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-py.test ./tests/commit_stage/unit_tests/
